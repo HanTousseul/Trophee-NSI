fc8164d3cf30c50ad9a1f67d21b21eca43f7942c
HanTousseul <141479304+HanTousseul@users.noreply.github.com>
2024-02-27 13:15:51 +0000

    tiny improvements. i created a loop that sets the color of the 900 pixels of the top right to black

--- Code.py
+++ Code.py
@@ -1,2 +1,9 @@
-#code
-print('hello word')
+from PIL import Image
+im=Image.open('/workspaces/Trophee-NSI/IMG_4939.png')
+px=im.load()
+coordinate = (1,1) 
+#print (im.getpixel(coordinate))
+for i in range(30):
+    for k in range(30):
+        im.putpixel((i,k),(0,0,0))
+im.show
